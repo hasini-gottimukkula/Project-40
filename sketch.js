ac157052738bb1fbea0307e8637c90b1b11acccc
hasini-gottimukkula <74771268+hasini-gottimukkula@users.noreply.github.com>
2021-03-23 16:44:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,20 +1,22 @@
 var database;
 var back_img;
-var gameState =0;
+var gameState = 0;
 var playerCount = 0;
 var allPlayers;
-var score =0;
-var player, form,game;
-var player1,player2;
+var score = 0;
+var player, form, game;
+var player1, player2;
 var players;
 var fruits;
 var fruitGroup;
 var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
 var player_img;
-var player1score =0;
-var player2score =0;
+var player1score = 0;
+var player2score = 0;
+var gameState = 0;
+var playCount = 0;
 
-function preload(){
+function preload() {
   back_img = loadImage("images/jungle.jpg");
   player_img = loadImage("images/basket2.png");
   fruit1_img = loadImage("images/apple2.png");
@@ -30,12 +32,22 @@ function setup() {
   game = new Game();
   game.getState();
   game.start();
-  
+
 }
 
 function draw() {
   background(back_img);
 
   // Add conditions for gameStates and playerCount
+  if (gameState === 1) {
+    clear();
+    game.play();
+  }
+  if (gameState === 2) {
+    game.end();
+  }
+  if (playerCount === 2) {
+    game.update(1);
+  }
 
 }
